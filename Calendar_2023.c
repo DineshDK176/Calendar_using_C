6363e73ed5eef08d6b5126c44d04f7560012a4d8
Dinesh_K <151514446+DineshDK176@users.noreply.github.com>
2025-04-11 10:07:22 +0530

    eff : Rearranging the code
    For the Better understanding rearrange the code and the comments are added..

--- Calendar_2023.c
+++ Calendar_2023.c
@@ -4,70 +4,66 @@ void days_name(int p);
 void no_days(int a);
 int x,count=0;
 
-void month()
+void month() // Function to show the name of the Months
 {
-	for(int i=1;i<=12;i++)
-    {	
+	for(int i=1;i<=12;i++) // For each case Each month name is printed
+    	{	
 		switch (i)
-        {
+        	{
 			case 1:
 			printf("\n\t  ---JANUARY---");
-            break;
-            case 2:printf("\n\n\t  ---FEBRUARY---");
-            break;
-            case 3:printf("\n\n\t  ---MARCH---");
-            break;
-            case 4:printf("\n\n\t  ---APRIL---");
-            break;
-            case 5:printf("\n\n\t  ---MAY---");
-            break;
-            case 6:printf("\n\n\t  ---JUNE---");
-            break;
-            case 7:printf("\n\n\t  ---JULY---");
-            break;
-            case 8:printf("\n\n\t  ---AUGUST---");
-            break;
-            case 9:printf("\n\n\t  ---SEPTEMBER---");
-            break;
-            case 10:printf("\n\n\t  ---OCTOBER---");
-            break;
-            case 11:printf("\n\n\t  ---NOVEMBER---");
-            break;
-            case 12:printf("\n\n\t  ---DECEMBER---");
-            break;
-        }
-		days_name(i);
-    }
+            		break;
+            		case 2:printf("\n\n\t  ---FEBRUARY---");
+            		break;
+            		case 3:printf("\n\n\t  ---MARCH---");
+            		break;
+            		case 4:printf("\n\n\t  ---APRIL---");
+            		break;
+            		case 5:printf("\n\n\t  ---MAY---");
+            		break;
+            		case 6:printf("\n\n\t  ---JUNE---");
+            		break;
+            		case 7:printf("\n\n\t  ---JULY---");
+            		break;
+            		case 8:printf("\n\n\t  ---AUGUST---");
+            		break;
+            		case 9:printf("\n\n\t  ---SEPTEMBER---");
+            		break;
+            		case 10:printf("\n\n\t  ---OCTOBER---");
+            		break;
+            		case 11:printf("\n\n\t  ---NOVEMBER---");
+            		break;
+            		case 12:printf("\n\n\t  ---DECEMBER---");
+            		break;
+        	}
+		days_name(i); // To print the Name of the Days
+    	}
 }
 
-void days_name(int p)
+void days_name(int p) // A function prints the Days
 {
-	char days[]="SUN  MON  TUE  WED  THU  FRI  SAT";
+	char days[]="SUN  MON  TUE  WED  THU  FRI  SAT"; // Days Name Stored in a Char Array
 	printf("\n\n%s\n---------------------------------\n",days);
-	no_days(p);
-    //printf("\n(days_name)x=%d\n",x);
+	no_days(p); // To print the Date of the each Months
 }
-void no_days(int a)
+void no_days(int a) // Function to print the date of each month
 {
 	int i,j=31;
-    if(count=1)
-    {
+    	if(count=1) // To print space b/w the date
+    	{
 		for(count;count<=x;count++)
-        {
+        	{
 			printf(" ");
-        }
-    }    
-    if(a==2)
+        	}
+    	}    
+    	if(a==2) // If it is a 2nd month it has 28 days
 		j=28;
-    //else if(a==2&&mod==0)    
-	//j=29;
-    else if(a==4||a==6||a==9||a==11)   
+    	else if(a==4||a==6||a==9||a==11)   // For these respective months have 30 days
 		j=30;
-    //printf("\nstart of days :x=%d\n",x);    
-    for(i=1,x;i<=j;x++)     
-    {   
+    	for(i=1,x;i<=j;x++)     
+    	{   
 		if(i<=9)
-        {
+        	{
 			if(x==1||x==6||x==11||x==16||x==21||x==26||x==31)
 			{
 				printf(" %d",i);
@@ -78,11 +74,11 @@ void no_days(int a)
 	 		else if(x==32)   
 			{
 				printf("\n");
-	    		x=0;
+	    			x=0;
 	 		}    
-        }
-        else
-        {	
+        	}
+        	else
+        	{	
 			if(x==1||x==6||x==11||x==16||x==21||x==26||x==31)
 			{
 				printf("%d",i);
@@ -93,15 +89,14 @@ void no_days(int a)
 	 		else if(x==32)   
 			{
 				printf("\n ");
-	    		x=0;
+	    			x=0;
 	 		}    
-        }
-
-    }
-    count=1;
- }
+        	}
+    	}
+    	count=1;
+}
 void main ()
 {
-printf("\t---CALANDER-2023---\n");
-	month();
+	printf("\t---CALANDAR-2023---\n"); // Title of the Calendar project
+	month(); // To print the Name of the Months
 }
